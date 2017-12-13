dd8ef53add09325c98e69bca1816a7bc2270a071
MMohan1 <manmohansharma987@gmail.com>
2017-12-13 15:56:17 +0530

    custom module changes

--- __init__.py
+++ __init__.py
@@ -1,3 +1,7 @@
+import os
 from run import TestClass
 tc = TestClass()
 from sub_module_routes import *
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+app_ini = "hirealchemy.ini"
+sub_module_ini = APP_ROOT + "/" + app_ini
