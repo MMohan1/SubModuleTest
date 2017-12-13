2ccef488126840571452c596eeb3058b29a267c5
MMohan1 <manmohansharma987@gmail.com>
2017-12-13 14:36:39 +0530

    ussses of hook manager

---
+++ __init__.py
@@ -0,0 +1,2 @@
+from run import TestClass
+tc = TestClass()
--- run.py
+++ run.py
@@ -1,2 +1,13 @@
+from routes import hm
+@hm.hook_function('hook_test_resistration_new')
 def Test():
-    return "Good To Know"
+    return "from custom module from Function"
+
+
+class TestClass:
+    def __init__(self):
+        pass
+
+    @hm.hook('hook_test_resistration')
+    def test(self):
+        return "from custom module"
