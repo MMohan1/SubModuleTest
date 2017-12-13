34add4d327e67a592214d5ab96d9446243db60c8
MMohan1 <manmohansharma987@gmail.com>
2017-12-13 18:30:10 +0530

    changes

--- __init__.py
+++ __init__.py
@@ -3,5 +3,5 @@ from run import TestClass
 tc = TestClass()
 from sub_module_routes import *
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-app_ini = "hirealchemy.ini"
+app_ini = "app.ini"
 sub_module_ini = APP_ROOT + "/" + app_ini
